73bb845120a9e4efedc81fb1cb5c93490ab53fa0
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-05-26 16:56:45 +0200

    add ticket price

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.log('JS OK');
 // chiedo all'utente il numero di km che deve percorrere
 // chiedo all'utente l'età
 
-const kmNum = prompt('Quanti km devi percorrere?' , 10);
-const userAge = prompt('Quanti anni hai?', 30);
+const kmNum = parseInt(prompt('Quanti km devi percorrere?' , 10));
+const userAge = parseInt(prompt('Quanti anni hai?', 30));
 console.log(kmNum , userAge);
 
 // calcolo il prezzo del biglietto
@@ -12,3 +12,9 @@ console.log(kmNum , userAge);
 const kmPrice = 0.21;
 const ticket = (kmNum * kmPrice) + '€';
 console.log(ticket);
+
+
+// SE l'utente ha meno di 18 anni va applicato uno sconto del 20%
+// SE l'utente ha 65 anni o più, va applicato uno sconto del 40%
+
+
