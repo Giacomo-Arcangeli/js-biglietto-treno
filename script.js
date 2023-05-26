9a9b8a6e19e09b26777c0f473e47c66d5ef7ec66
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-05-26 18:09:54 +0200

    fix total price

--- script.js
+++ script.js
@@ -4,27 +4,30 @@ console.log('JS OK');
 // chiedo all'utente l'età
 
 const kmNum = parseInt(prompt('Quanti km devi percorrere?' , 10));
-console.log(kmNum);
+console.log('Km:' , kmNum);
 const userAge = parseInt(prompt('Quanti anni hai?', 30));
-console.log(userAge);
+console.log('Anni:' , userAge);
 
 // calcolo il prezzo del biglietto
 
 const kmPrice = 0.21;
-const ticket = (kmNum * kmPrice) + '€';
-console.log(ticket);
-
-// calcolo gli sconti
-
-let discountJunior = 20;
-let discountSenior = 40;
-const ticketTotJunior = (ticket - (ticket / 100 * discountJunior));
-const ticketTotSenior = (ticket - (ticket / 100 * discountSenior));
+const ticket = (kmNum * kmPrice);
+console.log('Prezzo Base:' , ticket , '€');
 
 // SE l'utente ha meno di 18 anni va applicato uno sconto del 20%
 // SE l'utente ha 65 anni o più, va applicato uno sconto del 40%
 
 if(userAge < 18){
-    ticket === ticketTotJunior;
-    console.log(ticketTotJunior);
+    let discountJunior = 20;
+    // calcolo sconto
+    const ticketTotJunior = (ticket - (ticket / 100 * discountJunior));
+    // genero il prezzo totale del viaggio con lo sconto applicato
+    console.log('Prezzo Junior:' , parseFloat(ticketTotJunior).toFixed(2) , '€');
+}
+else if(userAge >= 65){
+    let discountSenior = 40;
+    // calcolo sconto
+    const ticketTotSenior = (ticket - (ticket / 100 * discountSenior));
+    // genero il prezzo totale del viaggio con lo sconto applicato
+    console.log('Prezzo Senior:' , parseFloat(ticketTotSenior).toFixed(2) , '€');
 }
